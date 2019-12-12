0de8c8d19ac401b61f3f77754a3658d40c9dc146
BaudhuinPie <baudhuin_pierre.pro@mailo.com>
2019-12-12 17:00:14 +0100

    Survey Form created by Pierre. -2- 17h00

--- front_app/src/Components/employee_surveys/KickOffPage.js
+++ front_app/src/Components/employee_surveys/KickOffPage.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react' ;
+import SurveyForm from './SurveyForm';
 
 export default class KickOffPage extends Component {
     render() {
@@ -6,7 +7,7 @@ export default class KickOffPage extends Component {
             <>
             <h1>Kick-off Survey</h1>
             <p>Wilcome on this survey. Description oncoming later. <b>Survey</b> form gonna be displayunder here</p>
-            <p> == questionnaire == </p>
+            <SurveyForm/>
             </>
         )
     }
---
+++ front_app/src/Components/employee_surveys/SurveyForm.js
@@ -0,0 +1,12 @@
+import React from 'react' ;
+
+const SurveyForm = (prop) => (
+    <form>
+        <p>block Question/réponse</p>
+        <p>block Question/réponse</p>
+        <p>block Question/réponse</p>
+        <button>Confirm</button>
+    </form>
+)
+
+export default SurveyForm;
