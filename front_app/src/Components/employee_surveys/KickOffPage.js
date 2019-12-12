486db80c6abc5dc0d2279e00c9b054c919c96295
BaudhuinPie <baudhuin_pierre.pro@mailo.com>
2019-12-12 16:47:30 +0100

    Survey Page component created by Pierre -1- 16h47.

---
+++ front_app/src/Components/employee_surveys/KickOffPage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react' ;
+
+export default class KickOffPage extends Component {
+    render() {
+        return(
+            <>
+            <h1>Kick-off Survey</h1>
+            <p>Wilcome on this survey. Description oncoming later. <b>Survey</b> form gonna be displayunder here</p>
+            <p> == questionnaire == </p>
+            </>
+        )
+    }
+}
