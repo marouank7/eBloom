cd17aad226b0015d16b2141cb83822e2894e8ecf
BaudhuinPie <baudhuin_pierre.pro@mailo.com>
2019-12-13 09:55:57 +0100

    before pulling by Pierre

--- front_app/src/App.js
+++ front_app/src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 import KickOffPage from './Components/employee_surveys/KickOffPage';
 
 function App() {
+
+  //RRRRRRRRRRRRRRRR___  Rendering  ___RRRRRRRRRRRRRRRRRRR
   return (
     <div className="App">
       <KickOffPage/>
     </div>
   );
+  //RRRRRRRRRRRRRRRRRRRR__ __RRRRRRRRRRRRRRRRRRRRRRRRRR
 }
 
 export default App;
