f2687311fe6c7fab37c16ed202574fa8990cd2b8
Marouan <marouan_k@live.be>
2019-12-17 10:57:12 +0100

    Composant checkbox, questionSurvey, stars, notImportant

--- front_app/src/actions/rateAction.js
+++
@@ -1,9 +0,0 @@
-/*
- src/actions/rateAction.js
-*/
-export const rateAction = (rating) => dispatch => {
-    dispatch({
-     type: 'RATE_ACTION',
-     payload: rating
-    })
-   }
--- front_app/src/reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-/*
- src/reducers/rootReducer.js
-*/
-import { combineReducers } from 'redux';
-
-import rateReducer from './rateReducer';
-export default combineReducers({
-    rateReducer
-});
