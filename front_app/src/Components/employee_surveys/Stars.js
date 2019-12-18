13350d557e6d4edf441dcbf2c7fd7115a6f9623b
BaudhuinPie <baudhuin_pierre.pro@mailo.com>
2019-12-18 16:24:08 +0100

    Star style added with Fontawesome. By Pierre -9- 16h24

--- front_app/src/Components/employee_surveys/Stars.js
+++ front_app/src/Components/employee_surveys/Stars.js
@@ -19,16 +19,16 @@ class Star extends Component {
     buildStarsListOf = (howWide, whichPoint) => {
         let aList = [   <div id={0} 
                                 className={whichPoint > 0 ?   "reset-bt possible" : "reset-bt hidden"}
-                                onClick={this.resetState} 
-                                
-                                
+                                onClick={this.resetState}     
                         > 
                             <span>&#60;</span> 
                         </div>
         ];
         const meanings = ["reset score","little agreement", "quite agree", "agree", "good agreement", "total agree"]
         for(let i = 1 ; i <= howWide ; i++) {
-            aList.push(  <div  id={i} title={meanings[i]} className={whichPoint >= i ? "star active" : "star" }></div> )
+            let adding = <div  className="star"><i id={i} class="fas fa-star fa" style={{"font-size":"30px","color":"white"}}></i> </div>;
+            if (whichPoint >= i) adding = <div  className="star"><i id={i} class="fas fa-star fa-2x" ></i> </div> ;
+                aList.push( adding )  //<div  id={i} title={meanings[i]} className={whichPoint >= i ? "star active" : "star" }></div>
         }
         return aList ;
    }
