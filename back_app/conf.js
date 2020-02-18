edc54f63451aaa695e9a98ddb57f23743797ecee
Pierre <pierregiddio@gmail.com>
2020-02-18 11:31:16 +0100

    Auto stash before merge of "Weekscategories" and "origin/Weekscategories"

--- back_app/conf.js
+++ back_app/conf.js
@@ -2,7 +2,11 @@ const  mysql = require('mysql');
 const  connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
+<<<<<<< Updated upstream
     password : '1Mamama!',
+=======
+    password : 'password',
+>>>>>>> Stashed changes
     database : 'Ebloom'
 });
 module.exports = connection;
